a3b0dc08ad9a192d0be1434df72f9d567c829cc8
Madhankannan02 <madhankannan29@gmail.com>
2025-12-28 10:26:57 +0530

    completed the project

--- src/Cube.jsx
+++ src/Cube.jsx
@@ -37,8 +37,11 @@ export function Cube(){
      rotationX,
      rotationY,
     });
-   }
-  })
+
+    startPointerX = currentPointerX;
+    startPointerY = currentPointerY;
+   },
+  });
  });
 
  return(
@@ -46,12 +49,12 @@ export function Cube(){
   <div className="h-screen bg-neutral-950 flex items-center justify-center">
    <div className="scene w-[300px] h-[300px] perspective-[1000px]">
     <div className="cube h-full w-full relative transform-3d -rotate-x-45 ">
-     <div className="front absolute w-[300px] h-[300px]"></div>
-     <div className="back absolute w-[300px] h-[300px]"></div>
-     <div className="right absolute w-[300px] h-[300px]"></div>
-     <div className="left absolute w-[300px] h-[300px]"></div>
-     <div className="bottom absolute w-[300px] h-[300px]"></div>
-     <div className="top absolute w-[300px] h-[300px]"></div>
+     <div className="front absolute w-[300px] h-[300px]  backdrop-blur-lg "></div>
+     <div className="back absolute w-[300px] h-[300px] backdrop-blur-lg "></div>
+     <div className="right absolute w-[300px] h-[300px] backdrop-blur-lg "></div>
+     <div className="left absolute w-[300px] h-[300px] backdrop-blur-lg "></div>
+     <div className="bottom absolute w-[300px] h-[300px] backdrop-blur-lg "></div>
+     <div className="top absolute w-[300px] h-[300px] backdrop-blur-lg "></div>
     </div>
    </div>
   </div>
